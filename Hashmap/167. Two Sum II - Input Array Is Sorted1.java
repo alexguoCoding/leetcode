386c35c306859cfe30b457c4ddae1d351c964418
songkunGuo <songkun.guo01@gmail.com>
2022-05-01 15:29:06 -0600

    Update 167. Two Sum II - Input Array Is Sorted1.java

--- Hashmap/167. Two Sum II - Input Array Is Sorted1.java
+++ Hashmap/167. Two Sum II - Input Array Is Sorted1.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        Map<Integer,Integer>map=new HashMap<>();
-        for (int i=0;i<numbers.length;i++){
-            int diff=target-numbers[i];
-            if(map.containsKey(diff)) {
-
-                 return new int[]{map.get(diff)+1, i+1};
+        int i=0,j=numbers.length-1;
+       
+        
+       while(numbers[i]+numbers[j]!=target){
+           if (numbers[i]+numbers[j]>target){
+               j--;
             }
             else{
-                map.put(numbers[i],i);
+               i++;
             }
-           
-            
+          
         }
-        
-            return null;
+ 
+             return new int[]{i+1, j+1};
+
 
         
     }
