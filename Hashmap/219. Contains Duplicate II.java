9cc9dd524c9e13c639d2e8b8232553d6f6f36596
songkunGuo <songkun.guo01@gmail.com>
2022-05-02 20:18:21 -0600

    Update 219. Contains Duplicate II.java

--- Hashmap/219. Contains Duplicate II.java
+++ Hashmap/219. Contains Duplicate II.java
@@ -1,20 +1,17 @@
 class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; ++i) {
-            if(set.contains(nums[i])){
-                return true;
-            }
-            else{
-                set.add(nums[i]);
-                    if(set.size()>k){
-                        set.remove(nums[i-k]);
-                        
+    public boolean containsNearbyDuplicate(int[] nums, int k){
+        
+        Set<Integer> set = new HashSet<>();
+         for (int i = 0; i < nums.length; i++) { 
+              if(!set.add(nums[i])){
+                  return true;
                 }
-            }
+
+            if(set.size()==k+1){
+                 set.remove(nums[i-k]);
+                   }
+
         }
-    return false;
-        
-        
+        return false;
     }
 }
