cf76f81c85f97c20f2531e54001046422e77ce4f
songkunGuo <songkun.guo01@gmail.com>
2022-05-12 11:01:35 -0600

    Update 495. Teemo Attacking.java

---
+++ array/495. Teemo Attacking.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int result=0;
+        int left=0;
+       
+         for (int i = 0; i < timeSeries.length-1; i++) {
+            result+= Math.min(timeSeries[i+1]-timeSeries[i],duration);
+
+        }
+             
+             
+        return result+duration;
+        
+        
+        
+    }
+}
