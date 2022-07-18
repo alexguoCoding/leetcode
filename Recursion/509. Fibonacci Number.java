2305b3d922803b784af0f2200b8b9c10b1c345c6
songkunGuo <songkun.guo01@gmail.com>
2022-07-18 06:48:38 -0600

    Update 509. Fibonacci Number.java

--- Recursion/509. Fibonacci Number.java
+++ Recursion/509. Fibonacci Number.java
@@ -1,16 +1,19 @@
 class Solution {
-    public int fib(int n) {
-        if(n==0){
-            return 0;
+
+    Map<Integer,Integer>map=new HashMap<>();
+        public int fib(int n) {
+             
+         if( map.containsKey(n)){
+             return map.get(n);
         }
-         if(n==1){
-            return 1;
+          if(n==1|n==0){
+              return n;
+            }
+           
+         int result= fib(n-1)+fib(n-2);
+             map.put( n,result);
+            return result;
+            
+    
         }
-        
-        
-        
-        
-        return  fib(n - 1) + fib(n - 2);
-        
     }
-}
